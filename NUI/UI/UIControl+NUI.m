cbd42387dd90ec34968ba50314f219a4ab05efa6
chenxGen <chenxGen@outlook.com>
2023-09-28 15:19:30 +0800

    fix: recursive loop of UIPageControll.

--- NUI/UI/UIControl+NUI.m
+++ NUI/UI/UIControl+NUI.m
@@ -33,6 +33,19 @@
 
 - (void)override_didMoveToWindow
 {
+    if (self.class != [UIControl class]) {
+        Method currentMethod = class_getInstanceMethod(self.class, @selector(override_didMoveToWindow));
+        Method expectedMethod = class_getInstanceMethod([UIControl class], @selector(override_didMoveToWindow));
+        if (currentMethod && expectedMethod) {
+            /// Both subclass and UIControl override method override_didMoveToWindow,
+            /// In this case, just invoke method of UIControl(original implementation of `didMoveToWindow`).
+            if (currentMethod != expectedMethod) {
+                ((void (*)(id, Method))method_invoke)(self, expectedMethod);
+                return;
+            }
+        }
+    }
+
     if (!self.isNUIApplied) {
         [self applyNUI];
     }
